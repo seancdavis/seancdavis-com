2b81dde4b72e253e304e288aad06c48d76ecc16d
Sean C Davis <scdavis41@gmail.com>
2020-06-09 08:01:16 -0400

    Header layout

--- .eleventy.js
+++ .eleventy.js
@@ -7,17 +7,24 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images")
 
   /**
+   * Reads a file in the _includes directory and returns the result.
+   */
+  const readIncludeFile = (filePath) => {
+    return fs.readFileSync(path.join(__dirname, `src/_includes/${filePath}`), "utf8")
+  }
+
+  /**
    * Renders component by passing in named arguments to the "component"
    * shortcode.
    */
-  eleventyConfig.addNunjucksShortcode("component", function (name, props) {
-    // Path to the component file.
-    const compFilePath = path.join(__dirname, `src/_components/${name}.njk`)
-    // Template content.
-    const rawComp = fs.readFileSync(compFilePath, "utf8")
-    // Pass the props to the component and render.
-    return nunjucks.renderString(rawComp, { ...props })
+  eleventyConfig.addNunjucksShortcode("component", (name, props) => {
+    return nunjucks.renderString(readIncludeFile(`components/${name}.njk`), { ...props })
   })
+
+  /**
+   * Reads an SVG from file and inserts its content directly on the page.
+   */
+  eleventyConfig.addNunjucksShortcode("svg", (name) => readIncludeFile(`svg/${name}.svg`))
 
   return {
     dir: {
