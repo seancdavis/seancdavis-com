9b3da5fa7f48e629ac23625b513d814fa0cb152a
Sean C Davis <scdavis41@gmail.com>
2020-08-02 07:33:16 -0400

    Image now renders a picture element

--- src/_includes/components/image/image.spec.js
+++ src/_includes/components/image/image.spec.js
@@ -1,5 +1,4 @@
 const transform = require("./image.transformer")
-const lodash = require("lodash")
 
 const breakpoints = {
   xs: {
@@ -29,54 +28,38 @@ describe("Image Transformer", () => {
     const result = transform({ class: "my-image", path: "/my-image.jpg" })
     expect(result.class).toEqual("my-image")
   })
-  it("builds out sources with sensible defaults", () => {
+  it("sources to the default max (x2 for retina) with default number of steps if no args passed", () => {
     const result = transform({ path: "/my-image.jpg" })
-    expect(result.sources[0].srcset).toContain(`${breakpoints.xs.max * 2}w`)
-    expect(result.sources[1].srcset).toContain(`${breakpoints.sm.max * 2}w`)
-    expect(result.sources[2].srcset).toContain(`${breakpoints.md.max * 2}w`)
-    expect(result.sources[3].srcset).toContain(`${breakpoints.lg.max * 2}w`)
-    expect(result.sources[4].srcset).toContain(`${breakpoints.xl.max * 2}w`)
-  })
-  it("generates 10 steps for each srcset", () => {
-    const result = transform({ path: "/my-image.jpg" })
+    expect(result.sources.length).toEqual(1)
     expect(result.sources[0].srcset.split(",").length).toEqual(10)
+    expect(result.sources[0].srcset).toContain(`${breakpoints.xl.max * 2}w`)
+  })
+  // This also implies that the array of sources is reversed.
+  it("sources to the next max value (x2 for retina)", () => {
+    const result = transform({ path: "/my-image.jpg", md: "400px" })
+    expect(result.sources.length).toEqual(2)
     expect(result.sources[1].srcset.split(",").length).toEqual(10)
-    expect(result.sources[2].srcset.split(",").length).toEqual(10)
-    expect(result.sources[3].srcset.split(",").length).toEqual(10)
-    expect(result.sources[4].srcset.split(",").length).toEqual(10)
+    expect(result.sources[1].srcset).toContain(`${breakpoints.md.min * 2}w`)
   })
   it("can adjust the number of steps", () => {
     const result = transform({ path: "/my-image.jpg", steps: 5 })
     expect(result.sources[0].srcset.split(",").length).toEqual(5)
-    expect(result.sources[1].srcset.split(",").length).toEqual(5)
-    expect(result.sources[2].srcset.split(",").length).toEqual(5)
-    expect(result.sources[3].srcset.split(",").length).toEqual(5)
-    expect(result.sources[4].srcset.split(",").length).toEqual(5)
   })
   it("will adjust srcset based on vw widths", () => {
     const result = transform({ path: "/my-image.jpg", lg: "50vw" })
-    expect(lodash.get(result, "sources[3].srcset")).toContain(breakpoints.lg.max)
-  })
-  it("creates an array of media queries representing the sources", () => {
-    const result = transform({ path: "/my-image.jpg" })
-    // Except not for the smallest sizes.
-    expect(result.sources[0].media).toEqual(undefined)
-    expect(result.sources[1].media).toEqual(`(min-width: ${breakpoints.sm.min}px)`)
-    expect(result.sources[2].media).toEqual(`(min-width: ${breakpoints.md.min}px)`)
-    expect(result.sources[3].media).toEqual(`(min-width: ${breakpoints.lg.min}px)`)
-    expect(result.sources[4].media).toEqual(`(min-width: ${breakpoints.xl.min}px)`)
-  })
-  it("creates an array of sizes representing the sources", () => {
-    const result = transform({ path: "/my-image.jpg", lg: "50vw" })
-    expect(result.sources[0].sizes).toEqual("100vw")
-    expect(result.sources[1].sizes).toEqual("100vw")
-    expect(result.sources[2].sizes).toEqual("100vw")
-    expect(result.sources[3].sizes).toEqual("50vw")
-    expect(result.sources[4].sizes).toEqual("100vw")
+    expect(result.sources[0].srcset).toContain(breakpoints.xl.max)
   })
   it("will only build two srcset values for a size sent a px value", () => {
     const result = transform({ path: "/my-image.jpg", md: "200px" })
-    expect(result.sources[2].srcset).toContain("200w")
-    expect(result.sources[2].srcset).toContain("400w")
+    expect(result.sources[0].srcset).toContain("200w")
+    expect(result.sources[0].srcset).toContain("400w")
+  })
+  it("generates a default src value as the max by default", () => {
+    const result = transform({ path: "/my-image.jpg" })
+    expect(result.src).toContain("1400")
+  })
+  it("generates a default src value as the max", () => {
+    const result = transform({ path: "/my-image.jpg", md: "1500px" })
+    expect(result.src).toContain("1500")
   })
 })
--- src/_includes/components/image/image.transformer.js
+++ src/_includes/components/image/image.transformer.js
@@ -18,34 +18,34 @@ const lodash = require("lodash")
  *
  */
 
-module.exports = ({
-  xs = "100vw",
-  sm = "100vw",
-  md = "100vw",
-  lg = "100vw",
-  xl = "100vw",
-  max = 1400,
-  steps = 10,
-  path,
-  ...props
-}) => {
+module.exports = ({ xs = "100vw", sm, md, lg, xl, max = 1400, steps = 10, path, ...props }) => {
   if (!path) {
     console.error("The path argument is required for transforming an image.")
     return props
   }
 
-  var client = new ImgixClient({
+  const client = new ImgixClient({
     domain: process.env.IMGIX_DOMAIN,
     secureURLToken: process.env.IMGIX_TOKEN
   })
 
+  let largestSrc = 0
+
   const generateUrl = params => client.buildURL(path, { auto: "format,compress", ...params })
 
   const generateSrcsets = widths => {
-    return widths.map(width => `${generateUrl({ w: width })} ${width}w`).join(",")
+    return widths
+      .map(width => {
+        width = parseInt(width)
+        if (width > largestSrc) largestSrc = width
+        return `${generateUrl({ w: width })} ${width}w`
+      })
+      .join(",")
   }
 
   const isPx = val => lodash.endsWith(val, "px")
+
+  const sizeArgs = [xs, sm, md, lg, xl]
 
   // These match Tailwind's breakpoints, with a passed-in max.
   let sizes = [
@@ -76,32 +76,40 @@ module.exports = ({
     }
   ]
 
-  let sources = [...sizes].map(({ min, max, size }) => {
+  const generateSources = (obj, idx) => {
+    if (idx > 0 && !obj.size) return null
+
     let output = {
-      sizes: size
+      sizes: obj.size
     }
 
-    if (min) {
-      output.media = `(min-width: ${min}px)`
+    if (obj.min) {
+      output.media = `(min-width: ${obj.min}px)`
     }
 
-    if (isPx(size)) {
-      const sizeInt = parseInt(size)
+    if (isPx(obj.size)) {
+      const sizeInt = parseInt(obj.size)
       output.srcset = generateSrcsets([sizeInt, sizeInt * 2])
     } else {
-      const pc = parseFloat(size) / 100.0
-      const actualMax = parseInt(max) * pc * 2
-      const actualMin = parseInt(min) * pc
+      const pc = parseFloat(obj.size) / 100.0
+
+      const nextSetSize = sizes.slice(idx + 1).find(x => x.size)
+
+      const actualMax = parseInt(nextSetSize ? nextSetSize.min : max) * pc * 2
+      const actualMin = parseInt(obj.min) * pc
       const stepLength = (actualMax - actualMin) / (steps - 1)
       const widths = Array.from({ length: steps }, (_, idx) => idx * stepLength + actualMin)
       output.srcset = generateSrcsets(widths)
     }
 
     return output
-  })
+  }
+
+  let sources = lodash.compact([...sizes].map(generateSources))
 
   return {
     ...props,
-    sources: sources
+    sources: lodash.reverse(sources),
+    src: generateUrl({ w: largestSrc / 2 })
   }
 }
