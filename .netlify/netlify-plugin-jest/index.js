ef6ac0190870f3ea11402d3737893b4679ccfc6b
Sean C Davis <scdavis41@gmail.com>
2020-08-02 13:34:30 -0400

    Remove posting status

--- .netlify/netlify-plugin-jest/index.js
+++ .netlify/netlify-plugin-jest/index.js
@@ -1,25 +1,8 @@
 module.exports = {
-  onPreBuild: async ({ utils: { build, run, status } }) => {
+  onPreBuild: async ({ utils: { build, run } }) => {
     try {
       await run.command("yarn test --ci")
-
-      status.show({
-        // Optional. Default to the plugin's name followed by a generic title.
-        title: "Continuous Integration",
-        // Required.
-        summary: "Tests passed"
-        // Optional. Empty by default.
-        // text: 'Detailed information shown in a collapsible section',
-      })
     } catch (error) {
-      status.show({
-        // Optional. Default to the plugin's name followed by a generic title.
-        title: "Continuous Integration",
-        // Required.
-        summary: "Tests failed"
-        // Optional. Empty by default.
-        // text: 'Detailed information shown in a collapsible section',
-      })
       return build.failBuild(error)
     }
   }
