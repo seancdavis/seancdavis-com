9e422dc721c4e1540c3650f84765685937d6b8ae
Sean C Davis <scdavis41@gmail.com>
2020-08-02 13:32:52 -0400

    Attempt to post status on CI build

--- .netlify/netlify-plugin-jest/index.js
+++ .netlify/netlify-plugin-jest/index.js
@@ -1,8 +1,25 @@
 module.exports = {
-  onPostBuild: async ({ utils: { build, run } }) => {
+  onPreBuild: async ({ utils: { build, run, status } }) => {
     try {
-      await run.command("yarn test")
+      await run.command("yarn test --ci")
+
+      status.show({
+        // Optional. Default to the plugin's name followed by a generic title.
+        title: "Continuous Integration",
+        // Required.
+        summary: "Tests passed"
+        // Optional. Empty by default.
+        // text: 'Detailed information shown in a collapsible section',
+      })
     } catch (error) {
+      status.show({
+        // Optional. Default to the plugin's name followed by a generic title.
+        title: "Continuous Integration",
+        // Required.
+        summary: "Tests failed"
+        // Optional. Empty by default.
+        // text: 'Detailed information shown in a collapsible section',
+      })
       return build.failBuild(error)
     }
   }
