07298d2ce8a5c42fd6b1c6d93638f3abee218712
Sean C Davis <scdavis41@gmail.com>
2020-08-07 06:45:24 -0400

    Fix accessibility and security issues

--- src/_includes/js/utils/scan-links.js
+++ src/_includes/js/utils/scan-links.js
@@ -4,6 +4,7 @@ onInit(() => {
   for (let link of links) {
     if (link.hostname.length && location.hostname !== link.hostname) {
       link.setAttribute("target", "_blank")
+      link.setAttribute("rel", "noopener")
     }
   }
 })
