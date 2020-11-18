eee797f2d318941d528f7cdf7783ccf43c6e52eb
Sean C Davis <scdavis41@gmail.com>
2020-11-18 06:25:32 -0500

    Overwrite slug filter

---
+++ utils/filters/slug.js
@@ -0,0 +1,23 @@
+const slugify = require("slugify")
+
+/**
+ * Converts a string to a lowercase, URL-friendly slug.
+ *
+ * @param {string} input The string to convert to a slug.
+ */
+exports.slugifyInput = input => {
+  if (!input) {
+    return false
+  }
+  return slugify(input.toString().toLowerCase(), { remove: /[*+~.()'"!:@?#]/g })
+}
+
+/**
+ * Add a prettier "slugify" filter. The "slug" filter does not remove colons,
+ * commas, apostrophes, etc. This does.
+ *
+ * @param {object} eleventyConfig Eleventy's configuration object
+ */
+exports.default = eleventyConfig => {
+  eleventyConfig.addFilter("slug", input => this.slugifyInput(input))
+}
---
+++ utils/filters/slug.spec.js
@@ -0,0 +1,20 @@
+const { slugifyInput } = require("./slug")
+
+describe("slugifyInput()", () => {
+  it("returns false when there is no input", () => {
+    const result = slugifyInput()
+    expect(result).toEqual(false)
+  })
+  it("converts the string to lower case", () => {
+    const result = slugifyInput("Hello World")
+    expect(result).toEqual("hello-world")
+  })
+  it("removes all unwanted characters", () => {
+    const result = slugifyInput("Hello *+~.()'\"!:@?# World")
+    expect(result).toEqual("hello-world")
+  })
+  it("strips lingering spaces", () => {
+    const result = slugifyInput("Hello  World ### ")
+    expect(result).toEqual("hello-world")
+  })
+})
