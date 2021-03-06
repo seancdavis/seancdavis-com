0939fc8bac6ad82047c7b8fd12a90e7d0fc7522a
Sean C Davis <scdavis41@gmail.com>
2021-03-06 15:41:20 -0500

    Run pnpm install after installing pnpm globally

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,6 +2,7 @@ module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
       await run.command("npm install -g pnpm")
+      await run.command("pnpm install")
     } catch (error) {
       return build.failBuild(error)
     }
