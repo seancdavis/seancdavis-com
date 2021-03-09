b822dbb9a631ae227c40cf2e1619d4e01f9666a1
Sean C Davis <scdavis41@gmail.com>
2021-03-09 10:15:53 -0500

    Don't freeze pnpm lock file

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,7 +2,7 @@ module.exports = {
   onPreBuild: async ({ utils: { build, run } }) => {
     try {
       await run.command("npm install -g pnpm")
-      await run.command("pnpm install")
+      await run.command("pnpm install --frozen-lockfile=false")
     } catch (error) {
       return build.failBuild(error)
     }
