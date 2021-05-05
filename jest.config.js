677da3ef2aedef862887208bb6beb9aeea185548
Sean C Davis <scdavis41@gmail.com>
2021-05-05 05:47:01 -0400

    Separate visual jest suite using config and env var

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  preset: "jest-puppeteer"
+process.env.JEST_CONFIG = process.env.JEST_CONFIG || "default"
+
+const config = {
+  default: {
+    testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/(.*).visual.(spec|test).[jt]s"]
+  },
+  visual: {
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+    testMatch: ["**/?(*.)+(visual).(spec|test).[jt]s?(x)"],
+    setupFilesAfterEnv: ["<rootDir>/.jest/visual-setup.js"],
+    reporters: ["default", "<rootDir>/.jest/image-reporter.js"],
+    preset: "jest-puppeteer"
+  }
 }
+
+module.exports = config[process.env.JEST_CONFIG]
