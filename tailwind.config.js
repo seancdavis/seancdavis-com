96330e8543539083b8683c8718db14614902fe3d
Sean C Davis <scdavis41@gmail.com>
2021-05-24 06:27:13 -0400

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,38 @@
 module.exports = {
-  future: {
-    purgeLayersByDefault: true
-  },
   purge: ["./src/**/*.html", "./src/**/*.njk", "./src/**/*.md"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px"
+    },
     container: {
       center: true,
       padding: "2rem"
     },
+    // fontSize: {
+    //   xs: '0.75rem',
+    //   sm: '0.875rem',
+    //   base: '1rem',
+    //   lg: '1.125rem',
+    //   xl: '1.25rem',
+    //   '2xl': '1.5rem',
+    //   '3xl': '1.875rem',
+    //   '4xl': '2.25rem',
+    //   '5xl': '3rem',
+    //   '6xl': '4rem',
+    // },
     extend: {
       colors: {
         black: "var(--color-black)",
         blue: {
-          default: "var(--color-blue)",
+          DEFAULT: "var(--color-blue)",
           dark: "var(--color-blue-dark)"
         },
         gray: {
           blue: "var(--color-gray-blue)",
-          default: "var(--color-gray-200)",
+          DEFAULT: "var(--color-gray-200)",
           100: "var(--color-gray-100)",
           200: "var(--color-gray-200)",
           300: "var(--color-gray-300)",
@@ -29,7 +44,7 @@ module.exports = {
           900: "var(--color-gray-900)"
         },
         green: {
-          default: "var(--color-green)"
+          DEFAULT: "var(--color-green)"
         },
         lime: "var(--color-lime)",
         orange: "var(--color-orange)",
