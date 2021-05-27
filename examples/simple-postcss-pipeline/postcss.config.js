24eb52c350ab6b88511e444c5e27ad6fa419200d
Sean C Davis <scdavis41@gmail.com>
2021-05-27 11:17:53 -0400

    Fix order in postcss config

--- examples/simple-postcss-pipeline/postcss.config.js
+++ examples/simple-postcss-pipeline/postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require("postcss-cssnext"),
     require("postcss-import"),
     require("postcss-nested"),
+    require("postcss-cssnext"),
     ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : [])
   ]
 }
