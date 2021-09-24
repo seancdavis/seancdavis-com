72854270125a67115708448fecf4c5295d9c22c4
Sean C Davis <scdavis41@gmail.com>
2021-09-24 06:27:13 -0400

    Move website files in a www directory

--- .netlify/netlify-plugin-jest/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  onPreBuild: async ({ utils: { build, run } }) => {
-    try {
-      await run.command("npm test --ci")
-    } catch (error) {
-      return build.failBuild(error)
-    }
-  }
-}
