7fa751edd5cd839b85c55f86b862f25ac2eb03e0
Sean C Davis <scdavis41@gmail.com>
2021-09-29 05:34:38 -0400

    Generate blank image

--- examples/generate-meta-images/generate-image.js
+++ examples/generate-meta-images/generate-image.js
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD");
+const { createCanvas } = require("canvas");
+const fs = require("fs");
+
+const width = 1200;
+const height = 627;
+
+const canvas = createCanvas(width, height);
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#764abc";
+context.fillRect(0, 0, width, height);
+
+const buffer = canvas.toBuffer("image/png");
+fs.writeFileSync("./image.png", buffer);
