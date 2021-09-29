761c4cbb3f35b2cdf8ed74d6b7cbaf82d99dac27
Sean C Davis <scdavis41@gmail.com>
2021-09-29 05:40:11 -0400

    Add byline

--- examples/generate-meta-images/generate-image.js
+++ examples/generate-meta-images/generate-image.js
@@ -4,13 +4,15 @@ const fs = require("fs");
 const { formatTitle } = require("./utils/format-title");
 
 const post = {
-  title: "Draw and save images",
+  title: "Draw and save images with Canvas and Node",
+  author: "Sean C Davis",
 };
 
 const width = 1200;
 const height = 627;
 const titleY = 170;
 const lineHeight = 100;
+const authorY = 500;
 
 const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
@@ -25,6 +27,9 @@ context.fillStyle = "#fff";
 const text = formatTitle(post.title);
 context.fillText(text[0], 600, titleY);
 if (text[1]) context.fillText(text[1], 600, titleY + lineHeight);
+
+context.font = "40pt 'PT Sans'";
+context.fillText(`by ${post.author}`, 600, authorY);
 
 const buffer = canvas.toBuffer("image/png");
 fs.writeFileSync("./image.png", buffer);
