ef984543ff46e59c78d475a6d7586009fa0dafdb
Sean C Davis <scdavis41@gmail.com>
2021-09-29 05:35:08 -0400

    Add post title

--- examples/generate-meta-images/generate-image.js
+++ examples/generate-meta-images/generate-image.js
@@ -3,12 +3,20 @@ const fs = require("fs");
 
 const width = 1200;
 const height = 627;
+const post = {
+  title: "Draw and save images with Canvas and Node",
+};
 
 const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
 
 context.fillStyle = "#764abc";
 context.fillRect(0, 0, width, height);
+
+context.font = "bold 70pt 'PT Sans'";
+context.textAlign = "center";
+context.fillStyle = "#fff";
+context.fillText(post.title, 600, 170);
 
 const buffer = canvas.toBuffer("image/png");
 fs.writeFileSync("./image.png", buffer);
