59caec5ff2fdc6ddcb0d52a66209df652d9ac389
Sean C Davis <scdavis41@gmail.com>
2021-09-29 05:37:41 -0400

    Add title formatter

--- examples/generate-meta-images/generate-image.js
+++ examples/generate-meta-images/generate-image.js
@@ -1,11 +1,16 @@
 const { createCanvas } = require("canvas");
 const fs = require("fs");
 
-const width = 1200;
-const height = 627;
+const { formatTitle } = require("./utils/format-title");
+
 const post = {
   title: "Draw and save images with Canvas and Node",
 };
+
+const width = 1200;
+const height = 627;
+const titleY = 170;
+const lineHeight = 100;
 
 const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
@@ -16,7 +21,10 @@ context.fillRect(0, 0, width, height);
 context.font = "bold 70pt 'PT Sans'";
 context.textAlign = "center";
 context.fillStyle = "#fff";
-context.fillText(post.title, 600, 170);
+
+const text = formatTitle(post.title);
+context.fillText(text[0], 600, titleY);
+if (text[1]) context.fillText(text[1], 600, titleY + lineHeight);
 
 const buffer = canvas.toBuffer("image/png");
 fs.writeFileSync("./image.png", buffer);
