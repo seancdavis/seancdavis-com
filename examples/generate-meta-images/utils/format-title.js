d9abe9cf29c6e9e1283bbdb33922a4eba18149e9
Sean C Davis <scdavis41@gmail.com>
2021-09-29 05:39:27 -0400

    Use up to 20 characters per line

--- examples/generate-meta-images/utils/format-title.js
+++ examples/generate-meta-images/utils/format-title.js
@@ -15,7 +15,7 @@ const getMaxNextLine = (input, maxChars = 20) => {
     if (prev?.done) return prev;
     const endLastWord = prev?.position || 0;
     const position = endLastWord + 1 + cur.length;
-    return position >= maxChars ? { done: true, index } : { position, index };
+    return position > maxChars ? { done: true, index } : { position, index };
   });
   // Using the index, build a string for this line ...
   const line = allWords.slice(0, lineIndex.index).join(" ");
@@ -36,7 +36,7 @@ exports.formatTitle = (title) => {
   let output = [];
   // If the title is 40 characters or longer, look to add ellipses at the end of
   // the second line.
-  if (title.length >= 40) {
+  if (title.length > 40) {
     const firstLine = getMaxNextLine(title);
     const secondLine = getMaxNextLine(firstLine.remainingChars);
     output = [firstLine.line];
@@ -47,7 +47,7 @@ exports.formatTitle = (title) => {
   // If 20 characters or longer, add the entire second line, using a max of half
   // the characters, making the first line always slightly shorter than the
   // second.
-  else if (title.length >= 20) {
+  else if (title.length > 20) {
     const firstLine = getMaxNextLine(title, title.length / 2);
     output = [firstLine.line, firstLine.remainingChars];
   }
