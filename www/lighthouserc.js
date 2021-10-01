8917177c4c4cc5ce17106f49c21ca66aaf4946b1
Sean C Davis <scdavis41@gmail.com>
2021-09-30 21:23:24 -0400

    [#181] lhci reads urls from the dist directory, if it exists

--- www/lighthouserc.js
+++ www/lighthouserc.js
@@ -1,31 +1,48 @@
-const runType = process.env.LHCI_RUN_TYPE || "mobile"
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+
+const runType = process.env.LHCI_RUN_TYPE || "mobile";
+
+// Determine the URLs to run the test against.
+const baseUrl = "https://www.seancdavis.com";
+const buildDir = path.join(__dirname, "./dist");
+let urls = [baseUrl];
+if (fs.existsSync(buildDir)) {
+  urls = glob.sync(path.join(buildDir, "**/*.html")).map((file) => {
+    let filePath = file.replace(buildDir, "").replace(".html", "");
+    if (filePath.endsWith("/index")) filePath = filePath.replace("/index", "/");
+    return `${baseUrl}${filePath}`;
+  });
+}
 
 const config = {
   default: {
     collect: {
       numberOfRuns: 1,
-      url: ["https://www.seancdavis.com/"]
+      // Use only up to the first 10 URLs.
+      url: urls.slice(0, 10),
     },
     upload: {
-      target: "temporary-public-storage"
-    }
-  }
-}
+      target: "temporary-public-storage",
+    },
+  },
+};
 
 config.mobile = {
-  ...config.default
-}
+  ...config.default,
+};
 
 config.desktop = {
   ...config.default,
   collect: {
-    url: config.default.collect.url.map(url => `${url}?desktop`),
+    url: config.default.collect.url.map((url) => `${url}?desktop`),
     settings: {
-      preset: "desktop"
-    }
-  }
-}
+      preset: "desktop",
+    },
+  },
+};
 
 module.exports = {
-  ci: config[runType]
-}
+  ci: config[runType],
+};
