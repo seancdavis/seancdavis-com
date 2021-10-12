028abec0004ba5eebadc38012ef810356609e161
Sean C Davis <scdavis41@gmail.com>
2021-10-12 06:17:02 -0400

    Add a couple diverse examples to visual tests

--- www/__tests__/integration.visual.spec.js
+++ www/__tests__/integration.visual.spec.js
@@ -1,7 +1,7 @@
-const { spawn } = require("child_process")
+const { spawn } = require("child_process");
 
-let serverProcess
-const port = "4567"
+let serverProcess;
+const port = "4567";
 
 const testPaths = [
   "/",
@@ -12,81 +12,89 @@ const testPaths = [
   "/blog/tag/javascript/",
   "/blog/tag/javascript/page-2/",
   // twitter embed, youtube embed, code block filename, code blocks, image (GIF)
-  // "/blog/three-ways-to-add-image-to-github-readme/",
+  "/blog/three-ways-to-add-image-to-github-readme/",
+  // back-to-back code blocks with filenames, youtube embed, note callout
+  "/blog/build-static-api-gatsby/",
+  // code blocsk with filenames, warning callout
+  "/blog/getting-started-with-postcss/",
   // top 5 posts
   "/blog/git-accept-merge-all-changes/",
   "/blog/wait-until-all-images-loaded/",
   "/blog/4-ways-to-pass-arguments-to-a-rake-task/",
   "/blog/change-css-iframe/",
-  "/blog/fix-yarn-integrity-check-failed/"
-]
+  "/blog/fix-yarn-integrity-check-failed/",
+];
 
-const buildUrl = path => {
-  const baseUrl = `http://localhost:${port}`
-  const url = new URL(path, baseUrl)
-  return url.toString()
-}
+const buildUrl = (path) => {
+  const baseUrl = `http://localhost:${port}`;
+  const url = new URL(path, baseUrl);
+  return url.toString();
+};
 
 const sleep = async (timeMs = 1000) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, timeMs)
-  })
-}
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeMs);
+  });
+};
 
 const shutdown = async () => {
   if (serverProcess) {
-    await serverProcess.kill()
+    await serverProcess.kill();
   }
-}
+};
 
 const waitForServer = async (count = 0) => {
   if (count >= 30) {
-    throw new Error("giving up on the server...")
+    throw new Error("giving up on the server...");
   }
 
   try {
-    const page = await browser.newPage()
-    await page.goto(buildUrl("/"))
+    const page = await browser.newPage();
+    await page.goto(buildUrl("/"));
   } catch (error) {
-    await sleep(1000)
-    await waitForServer(count + 1)
+    await sleep(1000);
+    await waitForServer(count + 1);
   }
-}
+};
 
 const startServer = async () => {
-  serverProcess = spawn("./node_modules/.bin/http-server", ["./dist", "--port", port])
+  serverProcess = spawn("./node_modules/.bin/http-server", [
+    "./dist",
+    "--port",
+    port,
+  ]);
 
-  await waitForServer()
-  await sleep()
+  await waitForServer();
+  await sleep();
 
-  return true
-}
+  return true;
+};
 
 describe("Visual Regression", () => {
   beforeAll(async () => {
-    await startServer()
-  })
+    await startServer();
+  });
 
   afterAll(async () => {
-    await shutdown()
-  })
+    await shutdown();
+  });
 
   for (let testPath of testPaths) {
     describe(testPath, () => {
       it("has not changed mobile rendering", async () => {
-        const page = await browser.newPage()
-        await page.setViewport({ width: 400, height: 800 })
-        await page.goto(buildUrl(testPath))
-        const image = await page.screenshot({ fullPage: true })
-        expect(image).toMatchImageSnapshot()
-      })
+        const page = await browser.newPage();
+        await page.setViewport({ width: 400, height: 800 });
+        await page.goto(buildUrl(testPath));
+        const image = await page.screenshot({ fullPage: true });
+        expect(image).toMatchImageSnapshot();
+      });
 
       it("has not changed desktop rendering", async () => {
-        const page = await browser.newPage()
-        await page.goto(buildUrl(testPath))
-        const image = await page.screenshot({ fullPage: true })
-        expect(image).toMatchImageSnapshot()
-      })
-    })
+        const page = await browser.newPage();
+        await page.goto(buildUrl(testPath));
+        const image = await page.screenshot({ fullPage: true });
+        expect(image).toMatchImageSnapshot();
+      });
+    });
   }
-})
+});
