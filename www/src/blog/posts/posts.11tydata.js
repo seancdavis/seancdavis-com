2de7fdd1fdab653e0703bff4fe0a34c05a00ad84
Sean C Davis <scdavis41@gmail.com>
2021-11-01 17:32:03 -0400

    Show related posts if they are mentioned in frontmatter

--- www/src/blog/posts/posts.11tydata.js
+++ www/src/blog/posts/posts.11tydata.js
@@ -2,5 +2,10 @@ module.exports = {
   permalink: "blog/{{ page.fileSlug }}/index.html",
   layout: "post",
   tags: ["Post"],
-  eleventyComputed: {}
+  eleventyComputed: {
+    related_posts: data => {
+      if (data.related_posts) return data.related_posts
+      return []
+    }
+  }
 }
