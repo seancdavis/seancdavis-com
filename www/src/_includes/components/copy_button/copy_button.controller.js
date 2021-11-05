cef8c708e2cce0aec80610d4a9d1482c96001e04
Sean C Davis <scdavis41@gmail.com>
2021-11-05 13:33:35 -0400

    Trigger alert from copy button controller

--- www/src/_includes/components/copy_button/copy_button.controller.js
+++ www/src/_includes/components/copy_button/copy_button.controller.js
@@ -4,5 +4,11 @@ onInit(() => {
   const selector = ".component--copy-button";
   const triggers = document.querySelectorAll(selector);
   if (triggers.length === 0) return;
-  new Clipboard(selector);
+  const clipboard = new Clipboard(selector);
+
+  clipboard.on("success", () => {
+    if (App?.Alert) {
+      App.Alert.show("URL copied to clipboard.");
+    }
+  });
 });
