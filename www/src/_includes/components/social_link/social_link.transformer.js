5b77f92f4a7955e7c53d4bc5fe691912c3b31269
Sean C Davis <scdavis41@gmail.com>
2021-11-05 20:56:41 -0400

    Fix a11y issues

--- www/src/_includes/components/social_link/social_link.transformer.js
+++ www/src/_includes/components/social_link/social_link.transformer.js
@@ -3,5 +3,5 @@ const { readSvg } = require("../../../../utils/shortcodes/svg");
 module.exports = ({ link, classes = "" }) => {
   const icon = readSvg(link.icon);
 
-  return { url: link.url, icon, classes };
+  return { url: link.url, icon, classes, name: link.icon };
 };
