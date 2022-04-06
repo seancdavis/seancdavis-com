16f7f2ffb0fa78262b278c91bfeb671f2816667e
Sean C Davis <scdavis41@gmail.com>
2022-04-06 06:51:07 -0400

    Mocks are working well Specs are failing because not handling rejects properly in Post.create()

--- packages/notion-post-publisher/__tests__/lib/Post.spec.ts
+++ packages/notion-post-publisher/__tests__/lib/Post.spec.ts
@@ -9,34 +9,36 @@ import {
 } from "../../src/utils/notion-utils";
 
 jest.mock("../../src/utils/notion-utils", () => {
-  // mockedGetAllPageBlocks.mockResolvedValue(mockPageBlocksApiResponse());
-  return {
-    getAllPageBlocks: async () => mockPageBlocksApiResponse(),
-    getPageProperties: async () => mockPagePropertiesResponse(),
-  };
+  return { getAllPageBlocks: jest.fn(), getPageProperties: jest.fn() };
 });
-// jest.mock("getPageProperties");
-// const mockedGetAllPageBlocks = notionUtils.getAllPageBlocks as jest.MockedFn<
-//   typeof notionUtils.getAllPageBlocks
-// >;
-// const mockedGetPageProperties = notionUtils.getPageProperties as jest.Mocked<
-//   typeof notionUtils.getPageProperties
-// >;
 
-// mockedGetAllPageBlocks.mockResolvedValue(mockPageBlocksApiResponse());
+const mockedGetAllPageBlocks = getAllPageBlocks as jest.MockedFn<
+  typeof getAllPageBlocks
+>;
+const mockedGetPageProperties = getPageProperties as jest.MockedFn<
+  typeof getPageProperties
+>;
 
 describe("Post", () => {
   describe("[static] .create()", () => {
     it("Creates an instance of Post", async () => {
+      mockedGetAllPageBlocks.mockResolvedValue(mockPageBlocksApiResponse());
+      mockedGetPageProperties.mockResolvedValue(mockPagePropertiesResponse());
       const post = await Post.create("SOME_PAGE_ID");
       expect(post instanceof Post).toBeTruthy();
     });
   });
   describe(".validate()", () => {
     it("Throws an error when no title", async () => {
+      mockedGetAllPageBlocks.mockResolvedValue(mockPageBlocksApiResponse());
+      mockedGetPageProperties.mockResolvedValue({
+        ...mockPagePropertiesResponse(),
+        title: "",
+      });
       const post = await Post.create("SOME_PAGE_ID");
-      // expect(post instanceof Post).toBeTruthy();
-      console.log(post.properties.title);
+      expect(post).rejects.toEqual(
+        "Notion Page SOME_PAGE_ID is missing a title."
+      );
     });
   });
 });
