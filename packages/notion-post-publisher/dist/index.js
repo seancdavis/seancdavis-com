97f9850498db3feb8765534bcdd4241e5e79717c
Sean C Davis <scdavis41@gmail.com>
2022-04-07 06:07:48 -0400

    Add comments to main function

--- packages/notion-post-publisher/dist/index.js
+++ packages/notion-post-publisher/dist/index.js
@@ -17,7 +17,12 @@ const Post_1 = require("./lib/Post");
 const notion_utils_1 = require("./utils/notion-utils");
 const chalk_1 = __importDefault(require("chalk"));
 /**
+ * Finds Notion pages in with the state "Draft: Ready", converts them to
+ * markdown posts with frontmatter properties, and writes them to file in the
+ * postsDir.
  *
+ * @param config Configuration object
+ * @param config.postsDir Absolute path to directory in which to write posts
  */
 function publishPosts(config) {
     return __awaiter(this, void 0, void 0, function* () {
--- packages/notion-post-publisher/src/index.ts
+++ packages/notion-post-publisher/src/index.ts
@@ -7,7 +7,12 @@ type InputConfig = {
 };
 
 /**
+ * Finds Notion pages in with the state "Draft: Ready", converts them to
+ * markdown posts with frontmatter properties, and writes them to file in the
+ * postsDir.
  *
+ * @param config Configuration object
+ * @param config.postsDir Absolute path to directory in which to write posts
  */
 export async function publishPosts(config: InputConfig) {
   const pageIds = await getPendingPageIds();
