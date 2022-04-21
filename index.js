b91def669e08d8d22a577d9bed8617648498e6a6
Sean C Davis <scdavis41@gmail.com>
2022-04-20 20:51:56 -0400

    Remvoe github example

--- index.js
+++
@@ -1,9 +0,0 @@
-export function extractGitHubRepoPath(url) {
-  if (!url) return null;
-  const match = url.match(
-    /^https?:\/\/(www\.)?github.com\/(?<owner>[\w.-]+)\/(?<repo>[\w.-]+)/
-  );
-
-  if (!match || !(match.groups?.owner && match.groups?.repo)) return null;
-  return `${match.groups.owner}/${match.groups.repo}`;
-}
