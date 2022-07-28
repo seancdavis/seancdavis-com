42dd7244db7ec0d240c0e92c6059220d6572d2c6
Sean C Davis <scdavis41@gmail.com>
2022-07-28 06:27:49 -0400

    Remove comments

--- packages/notion-post-publisher/dist/lib/blocks/EmbedBlock.js
+++ packages/notion-post-publisher/dist/lib/blocks/EmbedBlock.js
@@ -16,34 +16,9 @@ exports.EmbedBlock = void 0;
 const twitter_api_sdk_1 = require("twitter-api-sdk");
 const date_fns_1 = require("date-fns");
 const prettier_1 = __importDefault(require("prettier"));
-// TODO: This was a simple attempt beyond fetching from the Twitter API here.
-// But it's going to be much easier than trying to make all the rendering in the
-// website async.
-//
-// This might be a better lib to use:
-// https://github.com/twitterdev/twitter-api-typescript-sdk
-//
-// Previously, I left it at trying to determine the right project to use in the
-// developer portal. I created another one
-// (https://developer.twitter.com/en/portal/apps/24977852/settings) but thinking
-// I should delete it in favor of the production app
-// (https://developer.twitter.com/en/portal/projects/1453386255044075524/apps/23588172/settings).
-//
-// However, I have to make sure that the tweet publisher script still works in
-// the www project.
-// const embedRenderers: { [key: string]: (url: string) => string } = {
-//   "twitter.com": (url: string) => {
-//     // const matches = url.match(/\/status\/(?<id>\d+)/);
-//     // const id = matches?.groups?.id;
-//     // if (!id) throw new Error(`Could not identify tweet ID from URL: ${url}`);
-//     // return `{% twitter_embed id="${id}" %}`;
-//   },
-// };
 class EmbedBlock {
-    // url: string;
     constructor(params) {
         this.domain = new URL(params.embed.url).hostname;
-        // this.url = params.embed.url;
         if (this.domain in embedBlockMap) {
             this.embedBlock = new embedBlockMap[this.domain](params);
         }
@@ -60,10 +35,6 @@ class EmbedBlock {
             throw new Error(`Embed block not supported: ${this.domain}`);
         }
         return this.embedBlock.render();
-        // if (!(this.domain in embedRenderers)) {
-        //   throw new Error(`Embed block domain ${this.domain} not supported.`);
-        // }
-        // return embedRenderers[this.domain](this.url);
     }
 }
 exports.EmbedBlock = EmbedBlock;
--- packages/notion-post-publisher/src/lib/blocks/EmbedBlock.ts
+++ packages/notion-post-publisher/src/lib/blocks/EmbedBlock.ts
@@ -4,39 +4,12 @@ import prettier from "prettier";
 
 import type { NotionEmbedBlock } from "../../types/notion";
 
-// TODO: This was a simple attempt beyond fetching from the Twitter API here.
-// But it's going to be much easier than trying to make all the rendering in the
-// website async.
-//
-// This might be a better lib to use:
-// https://github.com/twitterdev/twitter-api-typescript-sdk
-//
-// Previously, I left it at trying to determine the right project to use in the
-// developer portal. I created another one
-// (https://developer.twitter.com/en/portal/apps/24977852/settings) but thinking
-// I should delete it in favor of the production app
-// (https://developer.twitter.com/en/portal/projects/1453386255044075524/apps/23588172/settings).
-//
-// However, I have to make sure that the tweet publisher script still works in
-// the www project.
-
-// const embedRenderers: { [key: string]: (url: string) => string } = {
-//   "twitter.com": (url: string) => {
-//     // const matches = url.match(/\/status\/(?<id>\d+)/);
-//     // const id = matches?.groups?.id;
-//     // if (!id) throw new Error(`Could not identify tweet ID from URL: ${url}`);
-//     // return `{% twitter_embed id="${id}" %}`;
-//   },
-// };
-
 export class EmbedBlock {
   domain: string;
   embedBlock?: TwitterEmbedBlock;
-  // url: string;
 
   constructor(params: NotionEmbedBlock) {
     this.domain = new URL(params.embed.url).hostname;
-    // this.url = params.embed.url;
     if (this.domain in embedBlockMap) {
       this.embedBlock = new embedBlockMap[this.domain](params);
     }
@@ -53,10 +26,6 @@ export class EmbedBlock {
       throw new Error(`Embed block not supported: ${this.domain}`);
     }
     return this.embedBlock.render();
-    // if (!(this.domain in embedRenderers)) {
-    //   throw new Error(`Embed block domain ${this.domain} not supported.`);
-    // }
-    // return embedRenderers[this.domain](this.url);
   }
 }
 
