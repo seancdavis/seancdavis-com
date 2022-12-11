16b9de5ca6b625a38e294ef940593144b82c16d9
Sean C Davis <scdavis41@gmail.com>
2022-12-11 07:48:07 -0500

    Remove postcss example

--- examples/simple-postcss-pipeline/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("postcss-nested"),
-    require("postcss-cssnext"),
-    ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : [])
-  ]
-}
